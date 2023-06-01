1dbf04d40d5ec6af9f1eeee17ece7036d541a659
Sandesh Hyoju <sundesz@gmail.com>
2023-06-01 12:02:05 +0300

    Change variable files

--- variables.tf
+++ variables.tf
@@ -4,87 +4,77 @@ variable "aws_region" {
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  default = "192.168.0.0/16"
 }
 
 variable "subnet_cidr" {
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+  default = ["192.168.1.0/24", "192.168.2.0/24", "192.168.11.0/24", "192.168.12.0/24"]
+  type    = list(string)
+}
+variable "private_cidrs" {
+  default = []
   type    = list(string)
 }
 
-variable "azs" {
+variable "availability_zone" {
   default = ["eu-north-1a", "eu-north-1b"]
   type    = list(string)
 }
 
-variable "ec2_type" {
-  default = "t3.micro"
-}
-
-variable "ec2_web_instance" {
-  default = "AWS Project Backend Server"
-}
-
-# variable "key_name" {
-#   default = "vockey"
-# }
-
-variable "rds_engine" {
-  type    = string
-  default = "mysql"
-}
-
-variable "rds_engine_version" {
-  type    = string
-  default = "8.0"
-}
-
-variable "rds_instance_class" {
-  type    = string
-  default = "db.t3.micro"
-}
-
-
-variable "rds_identifier" {
-  type    = string
-  default = "dbinstance"
-}
-
-
-variable "rds_db_name" {
-  type    = string
-  default = "employees"
-}
-
-
-variable "rds_username" {
-  type    = string
-  default = "ec2-user"
-  sensitive = true
-}
-
-variable "rds_password" {
-  type    = string
-  default = "admin"
-  sensitive = true
-}
 
 # This variable contains the configuration settings for the EC and RDS instances
+
 variable "settings" {
-  description = "Configuration settings"
-  type        = map(any)
+  type = object({
+    bastion_host = object({
+      name          = string
+      instance_type = string
+    })
+    web_server = object({
+      name          = string
+      instance_type = string
+    })
+    app_server = object({
+      name          = string
+      instance_type = string
+    })
+    database_instance = object({
+      allocated_storage   = number
+      engine              = string
+      engine_version      = string
+      instance_class      = string
+      identifier          = string
+      db_name             = string
+      username            = string
+      password            = string
+      skip_final_snapshot = bool
+    })
+  })
+
   default = {
-    "database" = {
-      engine              = "mysql"
-      engine_version      = "8.0.27"
-      instance_class      = "db.t2.micro"
-      db_name             = ""
-      skip_final_snapshot = true
-    },
-    "web_app" = {
-      instance_type = "t2.micro"
+    bastion_host = {
+      name          = "Bastion host"
+      instance_type = "t3.micro"
     }
+    web_server = {
+      name          = "Web server"
+      instance_type = "t3.micro"
+    }
+    app_server = {
+      name          = "Database server"
+      instance_type = "t3.micro"
+    }
+    database_instance = {
+      allocated_storage = 10
+      engine            = "mysql"
+      engine_version    = "8.0"
+      instance_class    = "db.t3.micro"
+      identifier        = "dbinstance"
+      db_name           = "employees"
+      username          = "main"
+      password          = "lab-password"
 
+      skip_final_snapshot = true
+    }
   }
-
 }
